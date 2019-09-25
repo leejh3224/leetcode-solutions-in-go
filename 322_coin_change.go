b6d92adf978eb8b35aec1c818273e1c8a82f977e
gompro <junhyung3224@gmail.com>
2019-09-25 23:00:51 +0900

    Update solution for #322

--- 322_coin_change.go
+++ 322_coin_change.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func min(a, b int) int {
@@ -17,35 +16,17 @@ func min(a, b int) int {
  * solution in Java: https://www.programcreek.com/2015/04/leetcode-coin-change-java/
  */
 func coinChange(coins []int, amount int) (ret int) {
-	if amount == 0 {
-		return -1
-	}
-
 	dp := make([]int, amount+1)
-	for i := range dp {
-		dp[i] = math.MaxInt64
-	}
-	dp[0] = 0
-
 	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
 		for _, coin := range coins {
-			// we can get here by 1 step
-			if i == coin {
-				dp[i] = 1
-			}
-
-			if i > coin {
-				// unreachable
-				if dp[i-coin] == math.MaxInt64 {
-					continue
-				}
-				// take min of prev step + 1 and value at i
+			if i >= coin {
 				dp[i] = min(dp[i-coin]+1, dp[i])
 			}
 		}
 	}
 
-	if dp[amount] == math.MaxInt64 {
+	if dp[amount] == amount+1 {
 		return -1
 	}
 	return dp[amount]
